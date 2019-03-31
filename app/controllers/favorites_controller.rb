a6bd4f620a47e5e3e091ba306bc8c4de5c2319a7
gorozo.ohtaki <gorozo5655@gmail.com>
2019-03-31 07:22:38 +0000

    stack01

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,18 @@
 class FavoritesController < ApplicationController
+  before_action :require_user_logged_in
+
   def create
+    post = Micropost.find(params[:micropost_id])
+    current_user.followpost(post)
+    flash[:success] = 'お気に入りに追加しました。'
+    redirect_to post
   end
 
   def destroy
+    post = Micropost.find(params[:micropost_id])
+    current_user.unfollowpost(post)
+    flash[:success] = 'お気に入りを解除しました。'
+    redirect_to post
   end
+  
 end
