0fa34241aa731cd9e5160a59a8637cedfbd9a1a4
gorozo.ohtaki <gorozo5655@gmail.com>
2019-03-31 06:24:08 +0000

    fav01/10.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,18 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :microposts
+  
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  
+  has_many :favorites
+  has_many :followingposts, through: :favorites, source: :micropost
+  has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'micropost_id'
+  has_many :followerposts, through: :reverses_of_favorite, source: :user
+  
+  
   
   def follow(other_user)
     unless self == other_user
